54b00e8f56dd0c1ca4798710a231ef3bb5d5823e
Andrei Cirlan <andreynho2006@gmail.com>
2021-04-21 22:24:53 +0300

    Initial Commit

---
+++ SlotsSwiftUI/SlotsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsSwiftUIApp.swift
+//  SlotsSwiftUI
+//
+//  Created by Andrei Cirlan on 21.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct SlotsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
